7109e425da9b304b344b3fcaa5e21b94a1e5ddf1
suginura <susuyuyu2021@gmail.com>
2026-06-16 00:09:08 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kokusen - Black Flash (JP/EN)
+
+練習として作ったFabric MODです。某漫画に登場する必殺の一撃を繰り出すことができます。
+
+A Fabric MOD I made as practice, that lets you perform a critical punch from a certain manga.
---
+++ src/client/java/io/github/suginura/kokusenblackflash/client/KokusenBlackFlashDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.suginura.kokusenblackflash.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KokusenBlackFlashDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
